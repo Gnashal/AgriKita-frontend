9b9a65b3a751cc59112f613f823589aebd39d1bb
RubyDEAD <reitzdave.andriano@gmail.com>
2025-04-13 11:55:32 +0800

    Please Ignore the last push
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/mobdev/agrikita/adapters/NewsAdapter.java
+++ app/src/main/java/mobdev/agrikita/adapters/NewsAdapter.java
@@ -1,22 +1,24 @@
-// NewsAdapter.java
 package mobdev.agrikita.adapters;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import com.kwabenaberko.newsapilib.models.Article;
 import mobdev.agrikita.R;
-import mobdev.agrikita.models.NewsArticle;
 
+
+// NewsAdapter.java
 public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
-    private List<NewsArticle> newsList = new ArrayList<>();
+    private List<Article> articles = new ArrayList<>();
 
     @NonNull
     @Override
@@ -28,27 +30,36 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull NewsViewHolder holder, int position) {
-        NewsArticle article = newsList.get(position);
+        Article article = articles.get(position);
         holder.titleTextView.setText(article.getTitle());
         holder.descriptionTextView.setText(article.getDescription());
+
+        // Load image using Glide or Picasso
+        if (article.getUrlToImage() != null) {
+            Glide.with(holder.itemView.getContext())
+                    .load(article.getUrlToImage())
+                    .into(holder.newsImageView);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return newsList.size();
+        return Math.min(articles.size(), 4); // Limit to 4 items
     }
 
-    public void setNewsList(List<NewsArticle> newsList) {
-        this.newsList = newsList;
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
         notifyDataSetChanged();
     }
 
     static class NewsViewHolder extends RecyclerView.ViewHolder {
+        ImageView newsImageView;
         TextView titleTextView;
         TextView descriptionTextView;
 
         public NewsViewHolder(@NonNull View itemView) {
             super(itemView);
+            newsImageView = itemView.findViewById(R.id.newsImage);
             titleTextView = itemView.findViewById(R.id.newsTitle);
             descriptionTextView = itemView.findViewById(R.id.newsDescription);
         }
