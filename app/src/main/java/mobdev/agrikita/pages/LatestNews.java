4473b163488f9fd3715d52c0237e6b0c480b8a98
RubyDEAD <reitzdave.andriano@gmail.com>
2025-04-13 12:10:42 +0800

    UPDATED!!! IGNORE THE 2 PUSH

--- app/src/main/java/mobdev/agrikita/pages/LatestNews.java
+++ app/src/main/java/mobdev/agrikita/pages/LatestNews.java
@@ -1,14 +1,11 @@
 package mobdev.agrikita.pages;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
