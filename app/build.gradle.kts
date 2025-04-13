611c5dd38bbe3db670c918bae551288b97a2f9fa
RubyDEAD <reitzdave.andriano@gmail.com>
2025-04-13 11:55:32 +0800

    Please Ignore the last push
    # Conflicts:
    #	.idea/misc.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,14 +38,14 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.car.ui.lib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.github.KwabenBerko:News-API-Java:1.0.2")
+    implementation ("com.github.bumptech.glide:glide:4.13.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.13.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation(platform("com.google.firebase:firebase-bom:33.12.0"))
     implementation("com.google.firebase:firebase-auth")
-
 }
