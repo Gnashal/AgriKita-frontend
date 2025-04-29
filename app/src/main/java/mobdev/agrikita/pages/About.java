0d1f64cfcebb44f8e9d9ea60cc80eea96f8edb83
RubyDEAD <retizdave.andriano@gmail.com>
2025-04-30 00:20:04 +0800

    about page done, added module page

--- app/src/main/java/mobdev/agrikita/pages/About.java
+++ app/src/main/java/mobdev/agrikita/pages/About.java
@@ -1,102 +1,51 @@
 package mobdev.agrikita.pages;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.activity.ComponentActivity;
-import androidx.activity.EdgeToEdge;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.appcompat.app.AppCompatActivity;
-
-
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-import com.bumptech.glide.Glide;
-import com.google.gson.Gson;
-
-import java.io.IOException;
 
 import mobdev.agrikita.R;
-import mobdev.agrikita.adapters.NewsAdapter;
-import mobdev.agrikita.models.auth.NewsApiResponse;
-import mobdev.agrikita.models.user.CurrentUser;
-import mobdev.agrikita.models.user.UserResponse;
-import mobdev.agrikita.controllers.UserService;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import mobdev.agrikita.R;
-
-
 
 public class About extends AppCompatActivity {
 
     private Button modulebutton;
-
     private LinearLayout marketplace;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);  // Fixed: changed clone to onCreate
-        EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // Make sure R.id.main exists in your XML
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        // Initialize views (make sure the IDs exist in XML)
         modulebutton = findViewById(R.id.module_btn);
+        marketplace = findViewById(R.id.mplace_layout);
 
-
-
-
-
-
+        // Do not call clicklisteners() to avoid null crashes
+         clicklisteners();
     }
-
 
     private void clicklisteners() {
+//         modulebutton.setOnClickListener(v ->
+////                 startActivity(new Intent(About.this, ModuleActivity.class))
+//         );
 
-        modulebutton.setOnClickListener(v -> {
-//            startActivity(new Intent(this, ));
-        });
-
-
-        marketplace.setOnClickListener(v -> {
-            //startActivity(new Intent(this, ));
-
-        });
-
+         marketplace.setOnClickListener(v ->
+                 startActivity(new Intent(About.this, Marketplace.class))
+         );
     }
-
-
-
-
 }
